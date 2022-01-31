8a85629a10e3075f2e637432941dbe3c8d96e179
Timothy Lyle <itsfreezingouthere@yahoo.com>
2022-01-31 16:57:50 -0500

    commit tree f08e26b08d79f44caa03bae3853e42ffd01e9392

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+function saturdayFun(reg = 'roller-skate'){
+  return `This Saturday, I want to ${reg}!`
+};
+
+function mondayWork(def = 'go to the office'){
+return `This Monday, I will ${def}.`
+};
+
+function wrapAdjective(s = "*"){
+  return function(f = 'special'){
+    return 'You are ' + s + f + s + '!'
+  }
+
+
+};
+
+
+
+/*function mondayWork(mondayDefault = 'go to the office') {
+  return `This Monday, I will ${mondayDefault}.`
+}
+saturdayFun()
+ 
+function wrapAdjective(f = '*') {
+  return function inner(p='special') {
+    return "You are " + f + p + f + "!"
+  }
+}*/
